08ddfb835aa1ad5eb80f08b942052470c63ad0ec
tai03 <68031973+taii03@users.noreply.github.com>
2024-06-08 01:17:13 -0300

    chore: :tada: First sunar commit

---
+++ tsup.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'tsup';
+
+import { esbuildPluginFilePathExtensions } from 'esbuild-plugin-file-path-extensions';
+import { esbuildPluginVersionInjector } from 'esbuild-plugin-version-injector';
+
+export default defineConfig({
+	entry: ['src/**/*.ts', '!src/**/*.spec.ts'],
+	outDir: 'build',
+	format: ['cjs', 'esm'],
+	clean: true,
+	dts: true,
+	treeshake: true,
+	target: 'es2022',
+	bundle: true,
+	sourcemap: true,
+	keepNames: true,
+	skipNodeModulesBundle: true,
+	esbuildPlugins: [
+		esbuildPluginVersionInjector(),
+		esbuildPluginFilePathExtensions({ esmExtension: 'js', cjsExtension: 'cjs' }),
+	],
+	ignoreWatch: ['**/node_modules/**', '**/.git/**'],
+});
