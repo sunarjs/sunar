b7dea5782613a234488cb36307e3eee82ec71ed4
tai03 <68031973+taii03@users.noreply.github.com>
2024-06-09 19:24:55 -0300

    refactor: :fire: remove unnecessary types

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,6 +1,2 @@
-import type { AnySelectMenuInteraction, ButtonInteraction, ModalSubmitInteraction } from 'discord.js';
-
 export type Prettify<TObject> = { [TKey in keyof TObject]: TObject[TKey] } & {};
 export type NextFunction = () => symbol;
-
-export type ComponentInteraction = ButtonInteraction | ModalSubmitInteraction | AnySelectMenuInteraction;
