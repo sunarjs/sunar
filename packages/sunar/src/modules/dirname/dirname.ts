40c7708968a50bf985645bd1259a95ceece1fcb7
Tai <github.discolor435@passinbox.com>
2024-07-09 02:05:03 -0300

    refactor: :recycle: use url as a parameter in dirname function

--- packages/sunar/src/modules/dirname/dirname.ts
+++ packages/sunar/src/modules/dirname/dirname.ts
@@ -1,8 +1,6 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-import { isESM } from '../isESM';
-
-export function dirname(): string {
-	return isESM() ? path.dirname(fileURLToPath(import.meta.url)) : __dirname;
+export function dirname(url: string): string {
+	return path.dirname(fileURLToPath(url))
 }
