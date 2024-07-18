a1a565dda28df1863aeba9400e63067c735e871e
Tai <github.discolor435@passinbox.com>
2024-07-17 23:11:23 -0300

    feat: :arrow_up: upgrade cli dependencies

--- packages/create-sunar/src/utils/dependencies.ts
+++ packages/create-sunar/src/utils/dependencies.ts
@@ -1,18 +1,18 @@
 export const DEPENDENCIES = {
-	sunar: '0.4.2',
+	sunar: '0.5.0',
 	'discord.js': '14.15.3',
 	dotenv: '16.4.5',
 
-	'@types/node': '20.14.9',
+	'@types/node': '20.14.11',
 	typescript: '5.5.3',
 
 	tsx: '4.16.2',
-	tsup: '8.1.0',
+	tsup: '8.1.2',
 	nodemon: '3.1.4',
 
 	'@biomejs/biome': '1.8.3',
 	eslint: '8.57.0',
 	'@typescript-eslint/parser': '7.14.1',
 	'@typescript-eslint/eslint-plugin': '7.14.1',
-	prettier: '3.3.2',
+	prettier: '3.3.3',
 } as const;
