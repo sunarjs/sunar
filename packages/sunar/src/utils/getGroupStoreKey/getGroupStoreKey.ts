18c141f5c5aa363262a3ff644d1bfa155c739147
Tai <github.discolor435@passinbox.com>
2024-10-07 22:49:07 -0300

    fix: :ambulance: fix getGroupKey

--- packages/sunar/src/utils/getGroupStoreKey/getGroupStoreKey.ts
+++ packages/sunar/src/utils/getGroupStoreKey/getGroupStoreKey.ts
@@ -3,8 +3,7 @@ import { isGroupBuilder } from '../isGroupBuilder';
 
 export function getGroupStoreKey(root: string | Group, parent?: string | null, sub?: string | null) {
 	if (isGroupBuilder(root)) {
-		return `${root.root}_${root.parent && root.sub ? `${root.parent}_${root.sub}` : root.sub}`;
+		return `${root.root}_${root.parent && root.sub ? `${root.parent}_${root.sub}` : root.parent}`;
 	}
-
 	return `${root}_${parent && sub ? `${parent}_${sub}` : sub}`;
 }
