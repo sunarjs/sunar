7ff23b80331304f75bf8a80b0472d1b4f161fdf2
adoist <adoist@duck.com>
2025-01-17 10:14:12 -0300

    style(biome): :art: update biome to 1.9.4

--- packages/sunar/tsup.config.ts
+++ packages/sunar/tsup.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
-import { esbuildPluginFilePathExtensions } from 'esbuild-plugin-file-path-extensions';
-import { esbuildPluginVersionInjector } from 'esbuild-plugin-version-injector';
+import { esbuildPluginFilePathExtensions } from "esbuild-plugin-file-path-extensions";
+import { esbuildPluginVersionInjector } from "esbuild-plugin-version-injector";
 
 export default defineConfig({
-	entry: ['src/**/*.ts', '!src/**/*.spec.ts', '!src/vitest/**/*.ts'],
-	format: ['cjs', 'esm'],
-	clean: true,
-	dts: true,
-	treeshake: true,
-	target: 'es2022',
-	bundle: true,
-	sourcemap: true,
-	keepNames: true,
-	skipNodeModulesBundle: true,
-	esbuildPlugins: [
-		esbuildPluginVersionInjector(),
-		esbuildPluginFilePathExtensions({ esmExtension: 'js', cjsExtension: 'cjs' }),
-	],
-	ignoreWatch: ['**/node_modules/**', '**/.git/**'],
+    entry: ["src/**/*.ts", "!src/**/*.spec.ts", "!src/vitest/**/*.ts"],
+    format: ["cjs", "esm"],
+    clean: true,
+    dts: true,
+    treeshake: true,
+    target: "es2022",
+    bundle: true,
+    sourcemap: true,
+    keepNames: true,
+    skipNodeModulesBundle: true,
+    esbuildPlugins: [
+        esbuildPluginVersionInjector(),
+        esbuildPluginFilePathExtensions({ esmExtension: "js", cjsExtension: "cjs" }),
+    ],
+    ignoreWatch: ["**/node_modules/**", "**/.git/**"],
 });
